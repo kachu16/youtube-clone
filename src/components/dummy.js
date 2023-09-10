8d17fba79483a34df5716d3491dbda5241e08f2c
Kajol Sharma <kajolsharma286@gmail.com>
2023-09-10 21:53:17 +0530

    secondcommmit

---
+++ src/components/dummy.js
@@ -0,0 +1,4 @@
+// git add dummy.js
+
+// main <<< master
+
---
+++ src/components/dummy2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dummy2 = () => {
+  return (
+    <div>
+      yoyo
+    </div>
+  )
+}
+
+export default dummy2
